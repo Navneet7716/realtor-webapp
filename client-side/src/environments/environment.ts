736835acdd1074638c9b97d24ff1201f023148c4
Navneet7716 <navneetsingh969@gmail.com>
2020-10-27 12:06:35 +0530

    changes in the front end with the navbar

--- client-side/src/environments/environment.ts
+++ client-side/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   mapbox: {
-    accessToken: 'Your Token'
+    accessToken: 'pk.eyJ1IjoibmF2bmVldDc3MTYiLCJhIjoiY2tnNmE0dnRtMTF1djJ3bDZlbzcwOXY1MSJ9.xOiL4qlZE6h4OSg5cMVyVQ'
   }
 };
 
